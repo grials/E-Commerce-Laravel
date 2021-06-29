adb82b444dd4c857cc18f2da1c49897c04e989b3
Grials <joseudemy@gmail.com>
2021-06-28 22:07:40 -0400

    prueba

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,10 @@ use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
-|--------------------------------------------------------------------------
+--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+routes are loaded by the RouteServiceProvider within a group which
 */
 
 Route::get('/', function () {
